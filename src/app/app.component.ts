a52119f454631f5f98100fe2107460a5505776d5
arutz <achim.rutz@googlemail.com>
2025-02-08 12:21:42 +0100

    chore: Refactor router setup and update component structure.
    Moved `<router-outlet>` inside a styled wrapper div for consistency. Removed direct reference to `UserProfileComponent` from the root component and defined proper routes for user-related components in `app.routes.ts`.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {UserProfileComponent} from './user-profile/user-profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UserProfileComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {UserProfileComponent} from './user-profile/user-profile.component';
+import {UsersOverviewComponent} from './users-overview/users-overview.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'users/profile', component: UserProfileComponent},
+  {path: 'users', component: UsersOverviewComponent}
+];
